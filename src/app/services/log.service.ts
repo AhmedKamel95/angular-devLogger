dd4c2e039057471f09d4e4379dd60a952ab195eb
ahmedkamel095 <ahmedkamel095@gmail.com>
2020-04-05 23:31:45 +0200

    Finished App

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,86 @@
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Log } from './../models/Log';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null,
+  });
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
+  constructor() {
+    // this.logs = [
+    //   {
+    //     id: '1',
+    //     text: 'Generated Components',
+    //     date: new Date('12/26/2017 12:54:23'),
+    //   },
+    //   {
+    //     id: '2',
+    //     text: 'Added Bootstrap',
+    //     date: new Date('12/27/2017 09:33:12'),
+    //   },
+    //   {
+    //     id: '3',
+    //     text: 'Added Logs Component',
+    //     date: new Date('12/27/2017 15:11:05'),
+    //   },
+    // ];
+    this.logs = [];
+  }
+
+  getLogs(): Observable<Log[]> {
+    if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+    return of(
+      this.logs.sort((a, b) => {
+        return b.date - a.date;
+      })
+    );
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  clearState() {
+    this.stateSource.next(true);
+  }
+}
